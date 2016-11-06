e0a69d0188c2d1969d09f1528fa9e85b1bf16357
Dan Osborne <djosborne10@gmail.com>
2016-11-05 19:48:13 -0700

    Updates.

--- two-tier/frontend/frontend.py
+++ two-tier/frontend/frontend.py
@@ -14,14 +14,9 @@ app = Flask(__name__, static_url_path='')
 def index():
 	rc = redis.Redis(host=REDIS_SERVER, port=6379, db=0)
 
-	raw_views = rc.incr('views')
-	if raw_views == None:
-		views = 1
-	else:
-		views = int(raw_views) + 1
-	rc.set('views', str(views))
+	views = rc.incr('views')
 
-	return "%s views! Reporting from task %s on host %s" % (views, os.getenv('MESOS_TASK_ID'), socket.gethostname())
+	return "%s views! Reporting from executor %s on host %s" % (views, os.getenv('MESOS_EXECUTOR_ID'), os.getenv('HOST'))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
